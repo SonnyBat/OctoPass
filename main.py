61bb745ad5f8d0d87ebc259c79c48ea14f1dae50
Sonny <sonny.software@outlook.com>
2026-08-24 13:34:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from tkinter import *
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
 symbols = ["!","#", "$", "%", "&", "(", ")", "*", "+"]
-combined = []
-password = ""
+
 
 
 
@@ -16,11 +15,9 @@ password = ""
 
 # functions
 def generate_password():
-    global combined # access global variable
-    global password # access global variable
-    password_input.delete(0,len(password)) # deletes password if generating new one
     combined = [] # makes combined list empty
     password = "" # makes password string empty
+    password_input.delete(0, END) # deletes password if generating new one
     for x in range (0, random.randint(6,10)): # creates random password looping through lists adding to combine then shuffel
         combined += random.choice(letters)
         combined += random.choice(numbers)
@@ -33,10 +30,10 @@ def generate_password():
 
 def add_details():
     with open("password.txt", "a") as file: # creates file called password.txt or adds to file if you already have it and adds user details you typed in
-        file.write( "\n" + "Website:" + website_input.get() + "\n" + "Email/User: " + email_input.get() + "\n" + "Password: " + password_input.get())
-
-
-
+        file.write( "\n" + "Website:" + website_input.get() + "\n" + "Email/User: " + email_input.get() + "\n" + "Password: " + password_input.get() + "\n")
+    password_input.delete(0, END)
+    email_input.delete(0, END)
+    website_input.delete(0, END)
 
 
 
@@ -63,6 +60,8 @@ try:
     window.iconbitmap("octoicon.ico")
 except:
     pass
+
+
 
 
 # Label
